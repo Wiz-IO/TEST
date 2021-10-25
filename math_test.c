a4747261dfe95462449ebe6223798b09548c814f
Georgi Angelov <the.wizarda@gmail.com>
2021-10-25 11:42:26 +0300

    Add files via upload

--- math_test.c
+++ math_test.c
@@ -77,4 +77,27 @@ gcc.exe (MinGW.org GCC-8.2.0-3) 8.2.0
 [6] dst = 9.282km
 [TEST-MATH] END
 
+TIRTOS Hello World
+[TEST-MATH] BEGIN
+[0] sum = 116.691290
+[1] atof1 = 116.123 atof2 = 0.567800 var = 58.000000 sum = 10.567800
+[2] atof1 = 116.123 atof2 = 0.567800 var = 58.000000
+[3] a = 0.85, b = 0.15, radLat1 = 31.110, radLat2 = 121.290, s = 9.00000
+[4] pos = 0.845889
+[5] a = 0.85, b = 0.15, radLat1 = 31.110, radLat2 = 121.290, s = 9.00000
+[6] dst = 9.282km
+[TEST-MATH] END
+
+BAREMETAL Hello World
+[TEST-MATH] BEGIN
+[0] sum = 116.691290
+[1] atof1 = 116.123 atof2 = 0.567800 var = 58.000000 sum = 10.567800
+[2] atof1 = 116.123 atof2 = 0.567800 var = 58.000000
+[3] a = 0.85, b = 0.15, radLat1 = 31.110, radLat2 = 121.290, s = 9.00000
+[4] pos = 0.845889
+[5] a = 0.85, b = 0.15, radLat1 = 31.110, radLat2 = 121.290, s = 9.00000
+[6] dst = 9.282km
+[TEST-MATH] END
+
+
 */
