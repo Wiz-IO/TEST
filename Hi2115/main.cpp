b916b9a9a26cf135da195ada53d33252394a573e
Georgi Angelov <the.wizarda@gmail.com>
2022-02-07 13:57:04 +0200

    Add files via upload

---
+++ Hi2115/main.cpp
@@ -0,0 +1,37 @@
+// BIN FILE SIZE: 71 kB
+
+#include <Arduino.h>
+
+#include <Modem.h>
+Modem dev;
+char imei[16];
+char imsi[16];
+
+void setup()
+{
+    dev.waitPinReady();
+
+    Serial.begin(115200);
+    printf("\n[APP] HiSilicon Hi2115 2022 Georgi Angelov\n\n");
+    pinMode(LED, OUTPUT);
+
+    dev.getIMEI(imei);
+    Serial.printf("[APP] IMEI: %s\n", imei);
+    dev.getIMSI(imsi);
+    Serial.printf("[APP] IMSI: %s\n", imsi);
+
+    // if(false == dev.waitRegistration()) reboot_system( REBOOT_CAUSE_APPLICATION_WATCHDOG );
+
+    // TCP/IP is ready
+    Serial.printf("[APP] Active Time: %d\n", (int)dev.active_time);
+    Serial.printf("[APP] Periodic TAU: %d\n", (int)dev.periodic_tau);
+}
+
+void loop()
+{
+    Serial.print("LOOP ");
+    digitalWrite(LED, 1);
+    delay(500);
+    digitalWrite(LED, 0);
+    delay(500);
+}
