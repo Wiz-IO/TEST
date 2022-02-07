fff97eafeff05939d0a88b907c8fa6d3d46d180f
Georgi Angelov <the.wizarda@gmail.com>
2022-02-07 14:20:49 +0200

    Add files via upload

--- Hi2115/main.cpp
+++ Hi2115/main.cpp
@@ -1,4 +1,4 @@
-// BIN FILE SIZE: 71 kB
+// BIN FILE SIZE: 68 kB
 
 #include <Arduino.h>
 
@@ -12,7 +12,7 @@ void setup()
     dev.waitPinReady();
 
     Serial.begin(115200);
-    Serial.println("\n[APP] HiSilicon Hi2115 2022 Georgi Angelov\n\n");
+    Serial.println("\n[APP] HiSilicon Hi2115 2022 Georgi Angelov\n");
     pinMode(LED, OUTPUT);
 
     dev.getIMEI(imei);
